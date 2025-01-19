1ab342890bcdf1d041958c7fee2e80b026b2c80f
Kholofelo Tladi <tladi.kholofelo@gmail.com>
2025-01-19 16:58:36 +0200

    Create components/layouts/Header.tsx

---
+++ alx-project-0x06/components/layouts/Layout.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react";
+import { LayoutProps } from "@/interface";
+import Footer from "./Footer";
+import Header from "./Header";
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
